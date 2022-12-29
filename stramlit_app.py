1860d8b075f4cde0f032fb92ef7215fe916331df
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2022-12-30 00:56:35 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('my parent new healthy dinner')
- streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
