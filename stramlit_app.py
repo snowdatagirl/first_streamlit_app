3236001c1f0daae445b770a39beb694030c12a48
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2022-12-30 00:45:00 +0530

    Update stramlit_app.py
    fixed error

--- stramlit_app.py
+++ stramlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('my parent new healthy dinner')
 streamlit.header('breakfast menu')
-streamlit.text(omege3 and blueberry oat meal)
-streamlit.text(kale,spinach and rocket smoothie)
-streamlit.text(hard boiled free range eggs)
+streamlit.text('omege3 and blueberry oat meal')
+streamlit.text('kale,spinach and rocket smoothie')
+streamlit.text('hard boiled free range eggs')
 
