db942586bfd967263dcfe74c0cc091043af978bb
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2022-12-30 16:59:52 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -12,6 +12,6 @@ streamlit.text('🥬🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🥑🧇avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import panda
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-stramlit.dataframe(my_fruit_list)             
+streamlit.dataframe(my_fruit_list)             
