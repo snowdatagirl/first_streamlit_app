9d06f29f29d720e77d0b7d44569d3c2b9c3856a1
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 04:35:37 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -23,7 +23,14 @@ fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
+def get_fruityvice_data(this_fruit_choice);
 
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   return fruityvice_normalized
+
+
+      
 streamlit.header("Fruityvice Fruit Advice!")
 
 try:
@@ -40,9 +47,9 @@ try:
     else:
 
          
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        back_from_function=get_fruityvice_data(fruit_choice)
+        
+        streamlit.dataframe(back_from_function)
          
          
 except URLError as e:
