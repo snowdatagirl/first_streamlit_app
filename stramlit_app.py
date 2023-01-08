da6a86a2aa9284476003bb25822f482477017329
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 05:10:46 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -78,4 +78,4 @@ if streamlit.button('Get Fruit Load List'):
 #fruit_choice = streamlit.text_input('What fruit would you like to add ?','Jackfruit')
 #streamlit.write('Thanks for adding ', fruit_choice)
 
-my_cur.execute("insert into fruit_load_list values('from streamlit')")
+#my_cur.execute("insert into fruit_load_list values('from streamlit')")
