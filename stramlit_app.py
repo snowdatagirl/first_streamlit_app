2500bbd653fa0b027fc3dbf641d2b1edc017fe2e
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 04:36:15 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -23,7 +23,7 @@ fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_list)
 
-def get_fruityvice_data(this_fruit_choice);
+def get_fruityvice_data(this_fruit_choice):
 
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
