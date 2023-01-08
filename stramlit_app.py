7a590fa545dea62e35213c4f87c778293fc666af
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 02:57:24 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLERROR
 
 streamlit.title('my parent new healthy dinner')
 streamlit.header('Breakfast Menu')
@@ -12,7 +16,7 @@ streamlit.text('🥬🥬Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🥑🧇avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
@@ -25,7 +29,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 
-import requests
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
@@ -34,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 streamlit.stop()
-import snowflake.connector
+#import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
