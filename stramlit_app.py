4270eb6a447d26c5da736fbcb3f3c8572fe9cf77
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 03:21:41 +0530

    Update stramlit_app.py

--- stramlit_app.py
+++ stramlit_app.py
@@ -34,13 +34,13 @@ try:
 
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+                  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+                   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+                   streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
 streamlit.stop()
 #import snowflake.connector
