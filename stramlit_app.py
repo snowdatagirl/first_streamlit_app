4fbe79e92c632e99646bf82dfe5c06ed6d0093d8
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 16:44:06 +0530

    Update stramlit_app.py
    else indention correction1

--- stramlit_app.py
+++ stramlit_app.py
@@ -28,7 +28,7 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("please select a fruit toget information.")
-    else:
+ else:
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       streamlit.dataframe(fruityvice_normalized)
