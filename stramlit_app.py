135440fe3c8920ce0759987c568920a864bcc91a
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 18:33:45 +0530

    Update stramlit_app.py
    made stop function a into comment

--- stramlit_app.py
+++ stramlit_app.py
@@ -39,7 +39,7 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.stop()   #dont run anything past here while we troubleshoot
+#streamlit.stop()   #dont run anything past here while we troubleshoot
 #import snowflake.connector
 
 
