6a020b00766126edc16efc1c77471beccade788e
Surabhi Pandey <120398877+surabhi363@users.noreply.github.com>
2023-01-09 19:52:53 +0530

    Update stramlit_app.py
    close added to 58 line

--- stramlit_app.py
+++ stramlit_app.py
@@ -55,6 +55,7 @@ def get_fruit_load_list():
 if streamlit.button('get fruit load list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close()
     streamlit.dataframe(my_data_rows)
     
 #allow the end user to add a fruit to the list
